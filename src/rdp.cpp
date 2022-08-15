c0d9990cbb1724b0969c6d0cb59ac8d602cc151f
Sean Maas <seanmaas27@gmail.com>
2022-08-14 20:01:28 -0400

    Flesh out RDP color formats and support RGBA32

--- src/rdp.cpp
+++ src/rdp.cpp
@@ -24,10 +24,20 @@
 #include "memory.h"
 #include "mi.h"
 
+enum Format
+{
+    RGBA4, RGBA8, RGBA16, RGBA32,
+    YUV4,  YUV8,  YUV16,  YUV32,
+    CI4,   CI8,   CI16,   CI32,
+    IA4,   IA8,   IA16,   IA32,
+    I4,    I8,    I16,    I32
+};
+
 struct Tile
 {
     uint16_t address;
     uint16_t width;
+    Format format;
 };
 
 namespace RDP
@@ -39,14 +49,20 @@ namespace RDP
     uint32_t endAddr;
     uint32_t status;
 
+    uint32_t addrBase;
+    uint32_t addrMask;
+
     uint32_t fillColor;
     uint32_t texAddress;
     uint16_t texWidth;
-    uint8_t texShift;
+    Format texFormat;
     uint32_t colorAddress;
     uint16_t colorWidth;
-    uint8_t colorShift;
+    Format colorFormat;
     Tile tiles[8];
+
+    uint32_t RGBA16toRGBA32(uint16_t color);
+    uint16_t RGBA32toRGBA16(uint32_t color);
 
     void runCommand();
     void texRectangle(uint64_t opcode);
@@ -81,19 +97,41 @@ void (*RDP::commands[0x40])(uint64_t) =
     unknown,      setTexImage, unknown,       setColorImage // 0x3C-0x3F
 };
 
+inline uint32_t RDP::RGBA16toRGBA32(uint16_t color)
+{
+    // Convert an RGBA16 color to RGBA32
+    uint8_t r = ((color >> 11) & 0x1F) * 255 / 31;
+    uint8_t g = ((color >>  6) & 0x1F) * 255 / 31;
+    uint8_t b = ((color >>  1) & 0x1F) * 255 / 31;
+    uint8_t a = (color & 0x1) ? 0xFF : 0x00;
+    return (r << 24) | (g << 16) | (b << 8) | a;
+}
+
+inline uint16_t RDP::RGBA32toRGBA16(uint32_t color)
+{
+    // Convert an RGBA32 color to RGBA16
+    uint8_t r = ((color >> 24) & 0xFF) * 31 / 255;
+    uint8_t g = ((color >> 16) & 0xFF) * 31 / 255;
+    uint8_t b = ((color >>  8) & 0xFF) * 31 / 255;
+    uint8_t a = (color & 0xFF) ? 0x1 : 0x0;
+    return (r << 11) | (g << 6) | (b << 1) | a;
+}
+
 void RDP::reset()
 {
     // Reset the RDP to its initial state
     startAddr = 0;
     endAddr = 0;
     status = 0;
+    addrBase = 0;
+    addrMask = 0;
     fillColor = 0;
     texAddress = 0;
     texWidth = 0;
-    texShift = 0;
+    texFormat = RGBA4;
     colorAddress = 0;
     colorWidth = 0;
-    colorShift = 0;
+    colorFormat = RGBA4;
     memset(tiles, 0, sizeof(tiles));
 }
 
@@ -148,6 +186,10 @@ void RDP::write(int index, uint32_t value)
                     status |= (1 << (i / 2));
             }
 
+            // Update the command address base and mask based on the RDRAM/DMEM bit
+            addrBase = (status & 0x1) ? 0xA4000000 : 0xA0000000;
+            addrMask = (status & 0x1) ? 0x00000FFF : 0x003FFFFF;
+
             // Keep track of unimplemented bits that should do something
             if (uint32_t bits = (value & 0x3C0))
                 LOG_WARN("Unimplemented RDP status bits set: 0x%X\n", bits);
@@ -161,11 +203,8 @@ void RDP::write(int index, uint32_t value)
 
 void RDP::runCommand()
 {
-    // Execute an RDP opcode from either RSP DMEM or RDRAM
-    uint32_t base = (status & 0x1) ? 0xA4000000 : 0xA0000000;
-    uint32_t mask = (status & 0x1) ? 0x00000FFF : 0x003FFFFF;
-    uint32_t address = base + (startAddr & mask);
-    uint64_t opcode = Memory::read<uint64_t>(address);
+    // Execute the RDP opcode at the command start address
+    uint64_t opcode = Memory::read<uint64_t>(addrBase + (startAddr & addrMask));
     (*commands[(opcode >> 56) & 0x3F])(opcode);
     startAddr = endAddr;
 }
@@ -175,19 +214,64 @@ void RDP::texRectangle(uint64_t opcode)
     // Decode the operands
     // TODO: handle the additional 64 bits of this command
     uint16_t y1 = ((opcode >>  0) & 0xFFF) >> 2;
-    uint16_t x1 = ((opcode >> 12) & 0xFFF) >> colorShift;
+    uint16_t x1 = ((opcode >> 12) & 0xFFF) >> 2;
     uint16_t y2 = ((opcode >> 32) & 0xFFF) >> 2;
-    uint16_t x2 = ((opcode >> 44) & 0xFFF) >> colorShift;
+    uint16_t x2 = ((opcode >> 44) & 0xFFF) >> 2;
     Tile &tile = tiles[(opcode >> 24) & 0x7];
 
-    // Draw a rectangle using a texture
-    for (uint16_t y = y1; y < y2; y++)
+    switch (colorFormat)
     {
-        for (uint16_t x = x1; x < x2; x += 4)
-        {
-            uint32_t value = *(uint32_t*)&tmem[(tile.address + (y - y1) * tile.width + (x - x1)) & 0xFFC];
-            Memory::write<uint32_t>(colorAddress + y * (colorWidth >> colorShift) + x, value);
-        }
+        case RGBA16:
+            switch (tile.format)
+            {
+                case RGBA16:
+                    // Draw a rectangle to an RGBA16 buffer using an RGBA16 texture
+                    for (uint16_t y = y1; y < y2; y++)
+                        for (uint16_t x = x1; x < x2; x++)
+                            Memory::write<uint16_t>(colorAddress + (y * colorWidth + x) * 2,
+                                *(uint16_t*)&tmem[(tile.address + (y - y1) * tile.width + (x - x1) * 2) & 0xFFE]);
+                    return;
+
+                case RGBA32:
+                    // Draw a rectangle to an RGBA16 buffer using an RGBA32 texture
+                    for (uint16_t y = y1; y < y2; y++)
+                        for (uint16_t x = x1; x < x2; x++)
+                            Memory::write<uint16_t>(colorAddress + (y * colorWidth + x) * 2, RGBA32toRGBA16(
+                                *(uint32_t*)&tmem[(tile.address + (y - y1) * tile.width * 2 + (x - x1) * 4) & 0xFFC]));
+                    return;
+
+                default:
+                    LOG_CRIT("Unknown RDP texture format: %d\n", tile.format);
+                    return;
+            }
+
+        case RGBA32:
+            switch (tile.format)
+            {
+                case RGBA16:
+                    // Draw a rectangle to an RGBA32 buffer using an RGBA16 texture
+                    for (uint16_t y = y1; y < y2; y++)
+                        for (uint16_t x = x1; x < x2; x++)
+                            Memory::write<uint32_t>(colorAddress + (y * colorWidth + x) * 4, RGBA16toRGBA32(
+                                *(uint16_t*)&tmem[(tile.address + (y - y1) * tile.width + (x - x1) * 2) & 0xFFE]));
+                    return;
+
+                case RGBA32:
+                    // Draw a rectangle to an RGBA32 buffer using an RGBA32 texture
+                    for (uint16_t y = y1; y < y2; y++)
+                        for (uint16_t x = x1; x < x2; x++)
+                            Memory::write<uint32_t>(colorAddress + (y * colorWidth + x) * 4,
+                                *(uint32_t*)&tmem[(tile.address + (y - y1) * tile.width * 2 + (x - x1) * 4) & 0xFFC]);
+                    return;
+
+                default:
+                    LOG_CRIT("Unknown RDP texture format: %d\n", tile.format);
+                    return;
+            }
+
+        default:
+            LOG_CRIT("Unknown RDP color buffer format: %d\n", colorFormat);
+            return;
     }
 }
 
@@ -201,43 +285,96 @@ void RDP::loadTile(uint64_t opcode)
 {
     // Decode the operands
     uint16_t t2 = ((opcode >>  0) & 0xFFF) >> 2;
-    uint16_t s2 = ((opcode >> 12) & 0xFFF) >> texShift;
+    uint16_t s2 = ((opcode >> 12) & 0xFFF) >> 2;
     uint16_t t1 = ((opcode >> 32) & 0xFFF) >> 2;
-    uint16_t s1 = ((opcode >> 44) & 0xFFF) >> texShift;
+    uint16_t s1 = ((opcode >> 44) & 0xFFF) >> 2;
     Tile &tile = tiles[(opcode >> 24) & 0x7];
 
-    // Copy a texture from the texture buffer to a tile address in TMEM
-    for (uint16_t t = t1; t < t2; t++)
+    // Only support loading textures without conversion for now
+    if (texFormat != tile.format)
     {
-        for (uint16_t s = s1; s < s2; s += 4)
-        {
-            uint32_t value = Memory::read<uint32_t>(texAddress + t * (texWidth >> texShift) + s);
-            *(uint32_t*)&tmem[(tile.address + (t - t1) * tile.width + (s - s1)) & 0xFFC] = value;
-        }
+        LOG_CRIT("Unimplemented RDP texture conversion: %d to %d\n", texFormat, tile.format);
+        return;
+    }
+
+    switch (texFormat & 0x3)
+    {
+        case 0x0: // 4-bit
+            // Copy a 4-bit texture from the texture buffer to a tile address in TMEM
+            // TODO: arrange the data properly
+            for (uint16_t t = t1; t <= t2; t++)
+                for (uint16_t s = s1; s <= s2; s += 2)
+                    tmem[(tile.address + (t - t1) * tile.width + (s - s1) / 2) & 0xFFF] =
+                        Memory::read<uint8_t>(texAddress + (t * texWidth + s) / 2);
+            return;
+
+        case 0x1: // 8-bit
+            // Copy an 8-bit texture from the texture buffer to a tile address in TMEM
+            // TODO: arrange the data properly
+            for (uint16_t t = t1; t <= t2; t++)
+                for (uint16_t s = s1; s <= s2; s++)
+                    tmem[(tile.address + (t - t1) * tile.width + (s - s1)) & 0xFFF] =
+                        Memory::read<uint8_t>(texAddress + t * texWidth + s);
+            return;
+
+        case 0x2: // 16-bit
+            // Copy a 16-bit texture from the texture buffer to a tile address in TMEM
+            // TODO: arrange the data properly
+            for (uint16_t t = t1; t <= t2; t++)
+                for (uint16_t s = s1; s <= s2; s++)
+                    *(uint16_t*)&tmem[(tile.address + (t - t1) * tile.width + (s - s1) * 2) & 0xFFE] =
+                        Memory::read<uint16_t>(texAddress + (t * texWidth + s) * 2);
+            return;
+
+        case 0x3: // 32-bit
+            // Copy a 32-bit texture from the texture buffer to a tile address in TMEM
+            // TODO: arrange the data properly and split across high and low banks
+            for (uint16_t t = t1; t <= t2; t++)
+                for (uint16_t s = s1; s <= s2; s++)
+                    *(uint32_t*)&tmem[(tile.address + (t - t1) * tile.width * 2 + (s - s1) * 4) & 0xFFC] =
+                        Memory::read<uint32_t>(texAddress + (t * texWidth + s) * 4);
+            return;
     }
 }
 
 void RDP::setTile(uint64_t opcode)
 {
     // Set parameters for the specified tile
-    // TODO: Handle bits 0-23, pixel size, and color format
+    // TODO: Actually use bits 0-23
     Tile &tile = tiles[(opcode >> 24) & 0x7];
     tile.address = (opcode >> 29) & 0xFF8;
     tile.width = (opcode >> 38) & 0xFF8;
+    tile.format = (Format)((opcode >> 51) & 0x1F);
 }
 
 void RDP::fillRectangle(uint64_t opcode)
 {
     // Decode the operands
     uint16_t y1 = ((opcode >>  0) & 0xFFF) >> 2;
-    uint16_t x1 = ((opcode >> 12) & 0xFFF) >> colorShift;
+    uint16_t x1 = ((opcode >> 12) & 0xFFF) >> 2;
     uint16_t y2 = ((opcode >> 32) & 0xFFF) >> 2;
-    uint16_t x2 = ((opcode >> 44) & 0xFFF) >> colorShift;
+    uint16_t x2 = ((opcode >> 44) & 0xFFF) >> 2;
 
-    // Draw a rectangle using the fill color
-    for (uint16_t y = y1; y < y2; y++)
-        for (uint16_t x = x1; x < x2; x += 4)
-            Memory::write<uint32_t>(colorAddress + y * (colorWidth >> colorShift) + x, fillColor);
+    switch (colorFormat)
+    {
+        case RGBA16:
+            // Draw a rectangle to an RGBA16 buffer using the fill color
+            for (uint16_t y = y1; y < y2; y++)
+                for (uint16_t x = x1; x < x2; x++)
+                    Memory::write<uint16_t>(colorAddress + (y * colorWidth + x) * 2, fillColor >> ((~x & 1) * 16));
+            return;
+
+        case RGBA32:
+            // Draw a rectangle to an RGBA32 buffer using the fill color
+            for (uint16_t y = y1; y < y2; y++)
+                for (uint16_t x = x1; x < x2; x++)
+                    Memory::write<uint32_t>(colorAddress + (y * colorWidth + x) * 4, fillColor);
+            return;
+
+        default:
+            LOG_CRIT("Unknown RDP color buffer format: %d\n", colorFormat);
+            return;
+    }
 }
 
 void RDP::setFillColor(uint64_t opcode)
@@ -249,19 +386,17 @@ void RDP::setFillColor(uint64_t opcode)
 void RDP::setTexImage(uint64_t opcode)
 {
     // Set the texture buffer parameters
-    // TODO: handle texture format bits
     texAddress = 0xA0000000 + (opcode & 0x3FFFFF);
-    texWidth = (((opcode >> 32) & 0x3FF) + 1) << 2;
-    texShift = ((opcode >> 51) & 0x3) ^ 0x3;
+    texWidth = ((opcode >> 32) & 0x3FF) + 1;
+    texFormat = (Format)((opcode >> 51) & 0x1F);
 }
 
 void RDP::setColorImage(uint64_t opcode)
 {
     // Set the color buffer parameters
-    // TODO: handle color format bits
     colorAddress = 0xA0000000 + (opcode & 0x3FFFFF);
-    colorWidth = (((opcode >> 32) & 0x3FF) + 1) << 2;
-    colorShift = ((opcode >> 51) & 0x3) ^ 0x3;
+    colorWidth = ((opcode >> 32) & 0x3FF) + 1;
+    colorFormat = (Format)((opcode >> 51) & 0x1F);
 }
 
 void RDP::unknown(uint64_t opcode)
